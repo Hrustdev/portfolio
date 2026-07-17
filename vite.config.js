1559c00144d8b6ec4cbafe0408c99d1af6bea534
Dmitry Hrustalev <120658632+Hrustdev@users.noreply.github.com>
2026-07-17 14:29:02 +0200

    1.1.2 patch (base for github)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
@@ -20,4 +20,5 @@ export default defineConfig({
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_DROP_MESSAGE_COMPILER__: false,
   },
-})
+  base: '/portfolio/',
+});
